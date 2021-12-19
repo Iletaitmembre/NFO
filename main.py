71bbf02e5ebe99d7d42a0cc17e3654a254b05983
Dimitri <88735045+Iletaitmembre@users.noreply.github.com>
2021-12-19 03:48:55 +0300

    Git и желтые окружности

---
+++ main.py
@@ -0,0 +1,48 @@
+from PyQt5.Qt import QColor
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton
+from PyQt5.QtGui import QPainter
+from random import randint
+
+
+class MainWindow(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.resize(600, 400)
+        self.initUI()
+
+    def initUI(self):
+        self.setGeometry(300, 300, 200, 200)
+        self.setWindowTitle('Рисование')
+        self.btn = QPushButton('Рисовать', self)
+        self.btn.move(70, 150)
+        self.do_paint = False
+        self.btn.clicked.connect(self.paint_func)
+
+    def paint_func(self):
+        self.do_paint = True
+        self.repaint()
+
+    def paintEvent(self, event):
+        if self.do_paint:
+            qp = QPainter()
+            qp.begin(self)
+            self.draw_flag(qp)
+            qp.end()
+
+    def draw_flag(self, qp):
+        qp.setBrush(QColor(255, 255, 0))
+        qp.drawEllipse(randint(20, 100), randint(20, 100), randint(20, 100), randint(20, 100))
+
+
+def except_hook(cls, exception, traceback):
+    sys.__excepthook__(cls, exception, traceback)
+
+
+if __name__ == "__main__":
+    import sys
+
+    app = QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
+    sys.excepthook = except_hook
+    sys.exit(app.exec_())
